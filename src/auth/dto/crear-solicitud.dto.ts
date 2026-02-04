a7cff018bc1a01d54004e1f81ad34f758e9a628f
PotatoesRandy <randyantigua149@gmail.com>
2026-02-04 10:12:35 -0400

    Fix: Agregar validadores al DTO CrearSolicitudDto

--- src/auth/dto/crear-solicitud.dto.ts
+++ src/auth/dto/crear-solicitud.dto.ts
@@ -1,6 +1,19 @@
+import { IsString, IsInt, IsNotEmpty } from 'class-validator';
+
 export class CrearSolicitudDto {
+  @IsString()
+  @IsNotEmpty()
   descripcion_solicitud: string;
+
+  @IsInt()
+  @IsNotEmpty()
   id_departamento: number;
+
+  @IsInt()
+  @IsNotEmpty()
   id_usuario: number;
+
+  @IsString()
+  @IsNotEmpty()
   usuario_accion: string;
 }
