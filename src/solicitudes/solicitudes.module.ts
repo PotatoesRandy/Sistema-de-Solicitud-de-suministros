ddb753107fa6db93dbbfb3e14337df5a311c1c75
PotatoesRandy <randyantigua149@gmail.com>
2026-02-04 09:50:34 -0400

    Fix: Registrar entidades Solicitud y SolicitudDetalle en módulo

--- src/solicitudes/solicitudes.module.ts
+++ src/solicitudes/solicitudes.module.ts
@@ -3,10 +3,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SolicitudesService } from './solicitudes.service';
 import { SolicitudesController } from './solicitudes.controller';
+import { Solicitud } from './entities/solicitud.entity';
+import { SolicitudDetalle } from './entities/solicitud-detalle.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([])], // Si usas entidades ORM
+  imports: [TypeOrmModule.forFeature([Solicitud, SolicitudDetalle])],
   controllers: [SolicitudesController],
   providers: [SolicitudesService],
+  exports: [SolicitudesService],
 })
 export class SolicitudesModule {}
